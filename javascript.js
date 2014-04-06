b34059bb44110d92b4dcc067a57cca7da5ba468a
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-04-07 00:45:22 +0100

    added hidden buttons/reveals when tweet box is clicked

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
 $('.new_tweet_input').focusin(function(){
-  $('.quick_tweet_box, .new_tweet_input').animate({height: '+=100px'},'fast');
+  $('.quick_tweet_box').animate({height: '+=100px'},'fast');
+  $('.new_tweet_input').animate({height: '+=50px'},'fast');
+  $('.hidden_buttons').show();
   $('.second_block').css('margin-top','110px')
 });
 
 $('.new_tweet_input').focusout(function(){
   $('.second_block').css('margin-top','0px')
-  $('.quick_tweet_box, .new_tweet_input').animate({height: '-=100px'},'fast');
+  $('.quick_tweet_box').animate({height: '-=100px'},'fast');
+  $('.new_tweet_input').animate({height: '-=50px'},'fast');
+   $('.hidden_buttons').hide();
 });
